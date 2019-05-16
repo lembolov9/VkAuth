505447be83608058dbc82419119c89196059622e
lembolov9 <Tomas080808>
2019-05-16 20:08:23 +0300

    change allowed hosts

--- VkAuth/settings.py
+++ VkAuth/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '=2mr!9=tvw(6678zk=szz39*gu+j$tdb66np*v%r$m!touaarg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
